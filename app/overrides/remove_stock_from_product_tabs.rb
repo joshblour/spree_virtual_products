be3914fe805f1e8414608c0d7610cd1302982480
Yonah Forst <joshblour@hotmail.com>
2014-01-21 20:27:23 +0100

    remove no shipping dependency

--- app/overrides/remove_stock_from_product_tabs.rb
+++ app/overrides/remove_stock_from_product_tabs.rb
@@ -3,3 +3,9 @@ Deface::Override.new(
   :remove => "erb[loud]:contains(\"link_to_with_icon 'icon-tasks', Spree.t(:stock_management), stock_admin_product_url(@product)\")",
   :name          => "remove_stock_management_from_product_tabs"
   )
+
+Deface::Override.new(
+  :virtual_path  => "spree/admin/variants/_form",
+  :remove => "[data-hook='admin_variant_form_additional_fields']",
+  :name          => "remove_admin_variant_form_additional_fields"
+  )
