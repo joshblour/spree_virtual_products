74da55fdae442d9f97dc04ce27a27eac8783212c
Yonah Forst <joshblour@hotmail.com>
2014-01-22 10:38:02 +0100

    initialize if defined

--- config/initializers/defaults.rb
+++ config/initializers/defaults.rb
@@ -1,2 +1,2 @@
-Spree::StockLocation.where(name: 'default', backorderable_default: true, propagate_all_variants: true).first_or_create
-Spree::ShippingCategory.where(name: 'default').first_or_create
+Spree::StockLocation.where(name: 'default', backorderable_default: true, propagate_all_variants: true).first_or_create if defined? Spree::StockLocation
+Spree::ShippingCategory.where(name: 'default').first_or_create if defined? Spree::ShippingCategory
